a74b15d0689c1934a674d2c58827aee08d33530e
Kritika <Kritikasagar1999@gmail.com>
2019-12-10 18:42:45 +0530

    initial commit

---
+++ LinkedList/reverse.cpp
@@ -0,0 +1,16 @@
+node* reverselist(node* head){
+	Node* temp;
+	Node* current = head;
+	Node* prev = NULL;
+
+	while(current!=NULL){
+		temp=current->next;
+		current->next=prev;
+		prev = current;
+		current = temp;
+
+	}
+	return Node* prev;
+
+
+}
