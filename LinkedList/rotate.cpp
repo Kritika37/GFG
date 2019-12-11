b2185f0e9e133b0840a7cac48836c665db1286ec
Kritika <Kritikasagar1999@gmail.com>
2019-12-11 10:59:30 +0530

    added new files

--- LinkedList/rotate.cpp
+++ LinkedList/rotate.cpp
@@ -1,3 +1,16 @@
 Node* rotate(Node* head, int k) {
+   Node* temp = head;
+   Node* current;
    
+   while(temp->next){
+   	temp=temp->next;
+   }
+   temp->next=head;
+
+   while(k--){
+   	current = head;
+   	head=head->next;
+   }
+   current->next=NULL;
+   return head;
 }
