56ddb212943ed83dde0ee2af2ecd1b742993f6d4
Kritika <Kritikasagar1999@gmail.com>
2020-01-11 20:35:48 +0530

    bt

--- Binarytree/bt.cpp
+++ Binarytree/bt.cpp
@@ -27,12 +27,31 @@ else{
 return rootptr;
 }
 
+bool search(node* rootptr,int data){
+	if(rootptr == NULL) return false;
+	else if(rootptr->data == data) return true;
+	else if(data <= rootptr->data) return search(rootptr->left,data);
+	else return search(rootptr->right,data);
+}
+
 
 int main(){
+int n;
 node* rootptr = NULL; // Pointer to root not root	
 rootptr = insert(rootptr,4);
 rootptr = insert(rootptr,8);
 rootptr = insert(rootptr,3);
+cout << "Number to be searched \n";
+cin >> n;
+if(search(rootptr,n) == true)
+{
+	cout << "Found\n";
 }
+else
+{
+	cout << "Not found\n";
+}
+}
+
 
 
