5b7f38999355b878cf6ce6ee6fa548f9adc98a95
Kritika <Kritikasagar1999@gmail.com>
2020-01-26 19:08:11 +0530

    BttoDLL previously i was using static which was working properly,so now with creating function appraoch

--- Binarytree/bttoDLL.cpp
+++ Binarytree/bttoDLL.cpp
@@ -1,19 +1,25 @@
-void bToDLL(Node *root, Node **head_ref)
+void helper(Node* root,Node **head_ref,Node** prev)
 {
-   if(root == nullptr) return;
+if(root==NULL)return;
 
-   static Node* prev = nullptr;
+helper(root->left,head_ref,prev);
 
-   bToDLL(root->left,head_ref);
-   if(prev == nullptr)
-   {
-   	*head_ref = root;
-   }
-   else
-   {
-   	root->left = prev;
-   	prev->right = root;
-   }
-   prev = root;
-   bToDLL(root->right,head_ref);
+if(*prev==NULL)
+{
+*head_ref=root;
+}
+else
+{
+root->left= *prev;
+(*prev)->right=root;
+}
+*prev=root;
+helper(root->right,head_ref,prev);
+}
+
+
+void bToDLL(Node *root, Node **head_ref)
+{  if(root == nullptr) return;
+    Node* prev = nullptr;
+   helper(root,head_ref,&prev);
 }
