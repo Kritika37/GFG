a91ce50245b9f1e223ba3f5e1cd89fdc00fd6289
Kritika <Kritikasagar1999@gmail.com>
2020-01-26 15:44:29 +0530

    connect nodes problems solved by level order traversal and connecting the nodes to nextright

---
+++ Binarytree/connectsamelevel.cpp
@@ -0,0 +1,29 @@
+void connect(Node* p){
+	if(p == nullptr) return;
+	
+		queue <Node *> q;
+		q.push(p);
+
+		while(!q.empty()){
+			int s = q.size(); //size of queue at each level
+			Node* prev = nullptr;
+
+			while(s--){
+			Node* temp = q.front();
+			q.pop();
+
+			if(temp->left != nullptr)
+				q.push(temp->left);
+
+			if(temp->right != nullptr)
+				q.push(temp->right);
+				if(prev!=NULL)
+				prev->nextRight=temp;
+
+				prev=temp;
+}
+
+				prev->nextRight=NULL;
+}
+
+}
